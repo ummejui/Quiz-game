52ed31aefbdd73d1afdd6e051b7b3c6010f58b64
ShornaJhorna <shorna662@gmail.com>
2021-04-04 14:20:50 +0600

    venv done

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!G:\Quiz_App_Project_using_Django_Framework\Quiz_App_Project_using_Django_Framework\Quiz_App_Project_using_Django_Framework\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
